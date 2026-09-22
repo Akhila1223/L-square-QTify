c6f1961f6cb8a040d109d76574d2ca8c965d11f6
Akhila Ch <akhilach11111@gmail.com>
2026-09-22 14:41:20 +0530

    fix: fix hero section

--- qtify/src/components/Hero/Hero.jsx
+++ qtify/src/components/Hero/Hero.jsx
@@ -8,6 +8,7 @@ function Hero() {
         <h1>100 Thousand Songs, ad-free</h1>
         <h1>Over thousands podcast episodes</h1>
       </div>
+
       <div>
         <img
           src={require("../../assets/hero_headphones.png")}
