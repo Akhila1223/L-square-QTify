6704e4d80d444a84345ea75571ccc4ac9b6a4eeb
Akhila Ch <akhilach11111@gmail.com>
2026-09-22 14:35:29 +0530

    feat: implement hero section

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 import "./App.css";
+import Hero from "./components/Hero/Hero.jsx";
 import Navbar from "./components/Navbar/Navbar.jsx";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Hero />
     </div>
   );
 }
